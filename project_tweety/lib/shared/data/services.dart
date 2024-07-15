98cb1c11867716f92b4b5c72b26edd1f5e096361
Euan <euan.scott@oldmutual.com>
2024-07-15 08:06:53 +0200

    Finalized services impl and testing

--- project_tweety/lib/shared/data/services.dart
+++ project_tweety/lib/shared/data/services.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
-import 'package:http/http.dart';
 
 /// TODO:
 /// Logging (dev purposes and to sentry.io/Firebase) - Put this rather in the code itself. This file should only be for making the service call. This way it can more easily be modularized if/when required.
@@ -25,7 +24,8 @@ class Services {
   ///
   /// [url]: The URL to send the POST request to.
   /// [body]: The data to include in the request body.
-  Future<Map<String, dynamic>> postData(String path, {
+  Future<Map<String, dynamic>> postData(
+    String path, {
     Map<String, String>? headers,
     required Map<String, dynamic> body,
     Encoding? encoding,
@@ -49,7 +49,8 @@ class Services {
   /// [url]: The URL to fetch data from.
   ///
   /// Returns either a Map<String, dynamic> representation of the JSON data or a List<Map<String, dynamic>> if the response is an array.
-  Future<dynamic> getData(String path, {
+  Future<dynamic> getData(
+    String path, {
     Map<String, String>? headers,
   }) async {
     var response = await sendHttpRequest('GET', path, headers: headers);
@@ -64,7 +65,8 @@ class Services {
   ///
   /// [url]: The URL to send the PUT request to.
   /// [body]: The data to include in the request body.
-  Future<Map<String, dynamic>> putData(String path, {
+  Future<Map<String, dynamic>> putData(
+    String path, {
     Map<String, String>? headers,
     required Map<String, dynamic> body,
     Encoding? encoding,
@@ -108,12 +110,13 @@ class Services {
   /// [encoding]: The encoding to use for the request body.
   ///
   /// Returns a [http.Response] object representing the response from the server.
-  Future<http.Response> sendHttpRequest(String method,
-      String path, {
-        Map<String, dynamic>? body,
-        Map<String, String>? headers,
-        Encoding? encoding = utf8,
-      }) async {
+  Future<http.Response> sendHttpRequest(
+    String method,
+    String path, {
+    Map<String, dynamic>? body,
+    Map<String, String>? headers,
+    Encoding? encoding = utf8,
+  }) async {
     // TODO: See about setting default headers here
 
     final url = Uri.parse(_baseUrl + path);
@@ -143,17 +146,26 @@ class Services {
     }
   }
 
-  /// Decodes a JSON string into a Map<String, dynamic>.
+  /// Decodes a JSON string into a dynamic data structure.
   ///
-  /// This function takes a JSON string as input and uses the `jsonDecode` function
-  /// from the `dart:convert` library to convert the JSON string into a Map<String, dynamic>.
-  /// The resulting Map<String, dynamic> is then returned.
+  /// This method takes a JSON string as input and uses the `jsonDecode` function
+  /// from the `dart:convert` library to convert the JSON string into a dynamic data structure.
+  /// The structure could be a `Map<String, dynamic>` for JSON objects or a `List<dynamic>`
+  /// for JSON arrays, depending on the structure of the input JSON string.
   ///
-  /// [data]: A JSON string to be decoded.
+  /// [data] A JSON string to be decoded.
   ///
-  /// Returns a Map<String, dynamic> representation of the JSON string.
+  /// Returns a dynamic representation of the decoded JSON string.
   dynamic _decodeJsonData(String data) => convert.jsonDecode(data);
 
+  /// Encodes a [Map<String, dynamic>] into a JSON string.
+  ///
+  /// This method takes a map containing data that you want to encode into a JSON string.
+  /// It uses the `jsonEncode` function from the `dart:convert` library to perform the encoding.
+  ///
+  /// [data] The map data to be encoded.
+  ///
+  /// Returns a JSON string representation of [data].
   _encodeJsonData(Map<String, dynamic> data) => convert.jsonEncode(data);
 
 //endregion
