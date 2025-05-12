8c2913322acada13196830792a04a260caeb1c7b
euanscott <euan@payflex.co.za>
2025-05-12 20:37:37 +0200

    Resolved dart analysis

--- project_tweety/lib/features/dynamic_form/data/data.dart
+++ project_tweety/lib/features/dynamic_form/data/data.dart
@@ -2,6 +2,7 @@
 ///
 /// By exporting these files, I make accessing the data classes easier within the dynamic_form feature.
 /// Historically for me, this has not been allowed as a common practice in the teams that I have worked on, I however do believe something like this makes for a better developer experience.
+library;
 
 export 'form_input_data.dart';
 export 'form_output_data.dart';
