d92e11dd65cdc9b085c3c1264a129bb673baa0a2
Euan Scott <euan@payflex.co.za>
2025-08-19 13:59:04 +0200

    docs: updated user docs

--- project_tweety/lib/data/constants/api_endpoints.dart
+++ project_tweety/lib/data/constants/api_endpoints.dart
@@ -1,13 +1,23 @@
 class ApiEndpoints {
   //region Authentication
+  /// POST - Creates a new user session
   static const String customerLogin = '/customer/login';
+
+  /// POST - Clear's the users session from the backend
   static const String customerLogout = '/customer/logout';
+
   //endregion
 
   //region Order
+  /// GET - Retrieves the customer's orders
   static const String customerOrders = '/customer/orders';
+
+  /// GET - Retrieve the details of a specific order
   static const String orderDetails = '/customer/orders/{orderId}';
 
+  /// Returns the endpoint URL for retrieving details of a specific order.
+  ///
+  /// [orderId] - The unique identifier of the order.
   static String orderDetailsUrl(String orderId) =>
       orderDetails.replaceAll('{orderId}', orderId);
   //endregion
