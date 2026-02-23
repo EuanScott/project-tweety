eb4d9304258ec7e91a1856faa2f60d9ef9fda375
Euan Scott <euan@payflex.co.za>
2026-02-23 09:11:58 +0200

    feat: app icon
    - Added new app icon

--- lib/presentation/blocs/home/home_bloc.dart
+++ lib/presentation/blocs/home/home_bloc.dart
@@ -5,25 +5,23 @@ import 'package:equatable/equatable.dart';
 import 'package:injectable/injectable.dart';
 import 'package:project_tweety/core/error_reporting/error_reporting_facade.dart';
 
-// part 'home_bloc.freezed.dart';
 part 'home_event.dart';
 part 'home_state.dart';
 
 @injectable
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
-  HomeBloc(/*this.errorReporting*/) : super(HomeInitial()) {
+  HomeBloc(this.errorReporting) : super(HomeInitial()) {
     on<HomeEvent>(_onHome);
-
-    final ErrorReportingFacade errorReporting;
   }
+
+  final ErrorReportingFacade errorReporting;
 
   Future<void> _onHome(HomeEvent event, Emitter<HomeState> emit) async {
     try {
       // do something here
     } catch (error, stacktrace) {
       // example usage
-      // unawaited(errorReporting.recordError(error, stacktrace));
-
+      unawaited(errorReporting.recordError(error, stacktrace));
       rethrow;
     }
   }
