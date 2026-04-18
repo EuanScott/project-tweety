db85339202890ed30fb895bfaf29a15d33fa9cc5
Euan Scott <euan@payflex.co.za>
2026-04-18 12:17:33 +0200

    chore: ui updates
    - Made 'home' contain shared widgets and emptied out settings

--- lib/presentation/pages/settings/settings.page.dart
+++ lib/presentation/pages/settings/settings.page.dart
@@ -1,16 +1,17 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:project_tweety/l10n/app_localizations.dart';
-import 'package:project_tweety/presentation/extensions/modal_extension.dart';
-import 'package:project_tweety/presentation/widgets/app_modal.dart';
 import 'package:project_tweety/presentation/widgets/page_scaffold.dart';
-import 'package:project_tweety/presentation/widgets/webview_modal.dart';
 
 class Settings extends StatelessWidget {
   const Settings({super.key});
 
   // TODO: Set it so that the i10n can be dynamically updated from the app and not always by changing the phone language in settings
+
+  // Global UI Bloc (Theme and Language)
+  // SharedPrederences to save what the user has set (not secure_storage)
+  // Set theme and language in a settings page
+  // Separate global bloc for business logic (auth and stuff)
+  // Updated agents for coding standards based off of cards page
 
   @override
   Widget build(BuildContext context) {
@@ -18,69 +19,7 @@ class Settings extends StatelessWidget {
 
     return PageScaffold(
       title: l10n.settingsTab,
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            TextButton(
-              child: const Text('Context Modal'),
-              onPressed: () {
-                context.showAppModal(
-                  const Center(child: Text('Modal content')),
-                );
-              },
-            ),
-            TextButton(
-              child: const Text('Page Modal'),
-              onPressed: () {
-                AppModal.page<bool>(
-                  context: context,
-                  child: const Center(child: Text('Modal content')),
-                );
-              },
-            ),
-            TextButton(
-              child: const Text('Blocking Modal'),
-              onPressed: () {
-                AppModal.blocking<bool>(
-                  context: context,
-                  child: Center(
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop(true);
-                      },
-                      child: const Text('Close Modal'),
-                    ),
-                  ),
-                );
-              },
-            ),
-            TextButton(
-              child: const Text('Compact Modal'),
-              onPressed: () async {
-                await AppModal.compact<bool>(
-                  context: context,
-                  maxHeightFactor: 0.35,
-                  child: const Center(child: Text('Modal content')),
-                );
-              },
-            ),
-            TextButton(
-              child: const Text('Blocking Modal'),
-              onPressed: () async {
-                final result = await WebviewModal.show(
-                  context,
-                  'https://euanscott.github.io/tester.html',
-                );
-
-                if (result != null) {
-                  log('User result: $result');
-                }
-              },
-            ),
-          ],
-        ),
-      ),
+      body: const Center(child: Text('Settings')),
     );
   }
 }
